32582e9d53140ac1615dd8c0826ef6d426ae5515
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-02-13 15:05:29 +0530

    updated

--- AI/Assignment2/A_star.py
+++ AI/Assignment2/A_star.py
@@ -5,12 +5,9 @@ def aStarAlgo(start_node, stop_node):
     g = {}               #store distance from starting node
     parents = {}         # parents contains an adjacency map of all nodes
     #distance of starting node from itself is zero
-
     g[start_node] = 0
     #start_node is root node i.e it has no parent nodes
-    #so start_node is set to its own parent node
     parents[start_node] = start_node
-
 
     while len(open_set) > 0:
         n = None
@@ -22,12 +19,9 @@ def aStarAlgo(start_node, stop_node):
         if n == stop_node or Graph_nodes[n] == None:
             pass
 
-
         # for intermediate node
         else:
             for (m, weight) in get_neighbors(n):
-                #nodes 'm' not in first and last set are added to first
-                #n is set its parent
                 # if we are encountering new node 
                 if m not in open_set and m not in closed_set:
                     open_set.add(m)
@@ -38,7 +32,6 @@ def aStarAlgo(start_node, stop_node):
                 else:
                     # if new node path is greater than previous then we are updating path with least cost
                     if g[m] > g[n] + weight:
-                        #update g(m)
                         g[m] = g[n] + weight
                         #change parent of m to n
                         parents[m] = n
@@ -51,8 +44,6 @@ def aStarAlgo(start_node, stop_node):
             print('Path does not exist!')
             return None
         
-
-
         # if the current node is the stop_node
         # then we begin reconstructin the path from it to the start_node
         if n == stop_node:
@@ -71,51 +62,12 @@ def aStarAlgo(start_node, stop_node):
     print('Path does not exist!')
     return None
 
-
-#define fuction to return neighbor and its distance
-#from the passed node
 def get_neighbors(v):
     if v in Graph_nodes:
         return Graph_nodes[v]
     else:
         return None
 
-'''
-#for simplicity we ll consider heuristic distances given
-#and this function returns heuristic distance for all nodes
-def heuristic(n):
-    H_dist = {
-        'A': 11,
-        'B': 6,
-        'C': 5,
-        'D': 7,
-        'E': 3,
-        'F': 6,
-        'G': 5,
-        'H': 3,
-        'I': 1,
-        'J': 0
-    }
-    return H_dist[n]
-
-#Describe your graph here
-Graph_nodes = {
-    'A': [('B', 6), ('F', 3)],
-    'B': [('A', 6), ('C', 3), ('D', 2)],
-    'C': [('B', 3), ('D', 1), ('E', 5)],
-    'D': [('B', 2), ('C', 1), ('E', 8)],
-    'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
-    'F': [('A', 3), ('G', 1), ('H', 7)],
-    'G': [('F', 1), ('I', 3)],
-    'H': [('F', 7), ('I', 2)],
-    'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
-}
-
-aStarAlgo('A', 'J')'''
-
-
-#for simplicity we ll consider heuristic distances given
-#and this function returns heuristic distance for all nodes
 def heuristic(n):
     H_dist = {
         'A': 11,
@@ -127,7 +79,6 @@ def heuristic(n):
     }
     return H_dist[n]
 
-#Describe your graph here
 Graph_nodes = {
     'A': [('B', 2), ('E', 3)],
     'B': [('A', 2), ('C', 1), ('G', 9)],
