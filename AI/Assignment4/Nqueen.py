efcf9947f7647702248fd2df5c6cb4437f4b9cfb
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-03-15 14:30:48 +0530

    updated

--- AI/Assignment4/Nqueen.py
+++ AI/Assignment4/Nqueen.py
@@ -1,111 +1,95 @@
-""" Python3 program to solve N Queen Problem
-using Branch or Bound """
+N = int(input("Enter no of queens: "))
 
-N = 8
+board = [[0]*N for _ in range(N)]
+def attack(i, j):
+  
+    for k in range(0,N):
+        if board[i][k]==1 or board[k][j]==1:
+            return True
+  
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k+l==i+j) or (k-l==i-j):
+                if board[k][l]==1:
+                    return True
+    return False
+def N_queens(n):
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            if (not(attack(i,j))) and (board[i][j]!=1):
+                board[i][j] = 1
+                if N_queens(n-1)==True:
+                    return True
+                board[i][j] = 0
+    return False
+N_queens(N)
+for i in board:
+    print (i)
 
-""" A utility function to print solution """
-def printSolution(board):
-	for i in range(N):
-		for j in range(N):
-			print(board[i][j], end = " ")
-		print()
 
-""" A Optimized function to check if
-a queen can be placed on board[row][col] """
-def isSafe(row, col, slashCode, backslashCode,
-		rowLookup, slashCodeLookup,
-					backslashCodeLookup):
-	if (slashCodeLookup[slashCode[row][col]] or
-		backslashCodeLookup[backslashCode[row][col]] or
-		rowLookup[row]):
-		return False
-	return True
 
-""" A recursive utility function
-to solve N Queen problem """
-def solveNQueensUtil(board, col, slashCode, backslashCode,
-					rowLookup, slashCodeLookup,
-					backslashCodeLookup):
-						
-	""" base case: If all queens are
-	placed then return True """
-	if(col >= N):
-		return True
-	for i in range(N):
-		if(isSafe(i, col, slashCode, backslashCode,
-				rowLookup, slashCodeLookup,
-				backslashCodeLookup)):
-					
-			""" Place this queen in board[i][col] """
-			board[i][col] = 1
-			rowLookup[i] = True
-			slashCodeLookup[slashCode[i][col]] = True
-			backslashCodeLookup[backslashCode[i][col]] = True
-			
-			""" recur to place rest of the queens """
-			if(solveNQueensUtil(board, col + 1,
-								slashCode, backslashCode,
-								rowLookup, slashCodeLookup,
-								backslashCodeLookup)):
-				return True
-			
-			""" If placing queen in board[i][col]
-			doesn't lead to a solution,then backtrack """
-			
-			""" Remove queen from board[i][col] """
-			board[i][col] = 0
-			rowLookup[i] = False
-			slashCodeLookup[slashCode[i][col]] = False
-			backslashCodeLookup[backslashCode[i][col]] = False
-			
-	""" If queen can not be place in any row in
-	this column col then return False """
-	return False
 
-""" This function solves the N Queen problem using
-Branch or Bound. It mainly uses solveNQueensUtil()to
-solve the problem. It returns False if queens
-cannot be placed,otherwise return True or
-prints placement of queens in the form of 1s.
-Please note that there may be more than one
-solutions,this function prints one of the
-feasible solutions."""
-def solveNQueens():
-	board = [[0 for i in range(N)]
-				for j in range(N)]
-	
-	# helper matrices
-	slashCode = [[0 for i in range(N)]
-					for j in range(N)]
-	backslashCode = [[0 for i in range(N)]
-						for j in range(N)]
-	
-	# arrays to tell us which rows are occupied
-	rowLookup = [False] * N
-	
-	# keep two arrays to tell us
-	# which diagonals are occupied
-	x = 2 * N - 1
-	slashCodeLookup = [False] * x
-	backslashCodeLookup = [False] * x
-	
-	# initialize helper matrices
-	for rr in range(N):
-		for cc in range(N):
-			slashCode[rr][cc] = rr + cc
-			backslashCode[rr][cc] = rr - cc + 7
-	
-	if(solveNQueensUtil(board, 0, slashCode, backslashCode,
-						rowLookup, slashCodeLookup,
-						backslashCodeLookup) == False):
-		print("Solution does not exist")
-		return False
-		
-	# solution found
-	printSolution(board)
-	return True
 
-# Driver Code
-print("Solution :")
-solveNQueens()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# global N
+# N = int(input('Enter Number of Queens: '))
+# def printSolution(board):
+#     for i in range(N):
+#         for j in range(N):
+#             print (board[i][j], end = " ")
+#         print()
+# def isSafe(board, row, col):
+#     for i in range(col):
+#         if board[row][i] == 1:
+#             return False
+ 
+#     for i, j in zip(range(row, -1, -1),
+#                     range(col, -1, -1)):
+#         if board[i][j] == 1:
+#             return False            
+#     for i, j in zip(range(row, N, 1),
+#                     range(col, -1, -1)):
+#         if board[i][j] == 1:
+#             return False
+#     return True
+# def solveNQUtil(board, col):
+#     if col >= N and board[0][1] == 1 and board[2][0] == 1:
+#         return True
+#     for i in range(N):
+#         if isSafe(board, i, col):
+#                          board[i][col] = 1
+#         if solveNQUtil(board, col + 1) == True:
+#                 return True
+#         board[i][col] = 0
+#     return False
+# def solveNQ(N):
+#     board = [[0 for j in range(N)]for i in range(N)]
+#     if solveNQUtil(board, 0) == False:
+#         print ("Solution does not exist")
+#         return False
+#     printSolution(board)
+#     return True
+#     solveNQ(N)
